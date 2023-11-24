ea8e0649303774231d2b28c463bcbbbc7a7d67dc
MatasN02 <90383891+MatasN02@users.noreply.github.com>
2023-11-24 11:00:27 +0200

    helper.cs

---
+++ helper.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUI
+{
+    public static class helper
+    {
+        public static string CnnVal(string name)
+        {
+           return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+    }
+}
